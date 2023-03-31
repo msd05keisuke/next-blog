d3b6edeadc36c05e5238f5ad4a1f5acaa7b3433b
増田圭佑 <masudakeisuke@masudakeisukenoMacBook-Pro.local>
2023-03-31 22:44:03 +0900

    first commit

---
+++ pages/blog/[id].tsx
@@ -0,0 +1,38 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext, NextPage } from 'next'
+import { client } from "@/src/libs/client";
+import { Blog } from "@/src/types/blog";
+
+const BlogId = (blog: Blog) => {
+
+    return (
+        <main>
+            <h1>{blog.title}</h1>
+            <p>{blog.publishedAt}</p>
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: `${blog.body}`,
+                }}
+            />
+        </main>
+    )
+}
+
+// 動的ルーティング時に利用
+export const getStaticPaths: GetStaticPaths = async () => {
+    const data = await client.get({ endpoint: "blog" });
+    const paths = data.contents.map((blog: Blog) => `/blog/${blog.id}`);
+    return { paths, fallback: false };
+};
+
+// getStaticPathsでreturnしたpaths内のオブジェクトを引数contextから参照
+export const getStaticProps = async (
+    context: GetStaticPropsContext<{ id: string }>
+) => {
+    const id = context.params?.id;
+    const data = await client.get({ endpoint: "blog", contentId: id });
+    return {
+        props: data,
+    };
+};
+
+export default BlogId;
